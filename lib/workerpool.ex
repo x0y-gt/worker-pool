430bb0a27c20a1c0d3d2c785b71ebe1cdbfc3418
x0y <x0y@milkyweb.co>
2018-03-09 11:27:57 -0600

    Initial commit

---
+++ lib/workerpool.ex
@@ -0,0 +1,18 @@
+defmodule Workerpool do
+  @moduledoc """
+  Documentation for Workerpool.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Workerpool.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
